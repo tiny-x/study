54323b8941ea075c6843a2e9ec75adda9aec44ff
小小 <185120555@qq.com>
2018-02-03 16:15:13 +0800

    basic test

--- basic/src/main/java/com/xy/basic/ByteModel.java
+++ basic/src/main/java/com/xy/basic/ByteModel.java
@@ -7,10 +7,62 @@ package com.xy.basic;
 public class ByteModel {
 
     public static void main(String[] args) {
-        System.out.println(15 % 4);
-        System.out.println(15 & (4 -1));
 
-        // 判断是否是2的次方
-        System.out.println(16 & (16 -1));
+        System.out.println(tableSizeFor(151));
+    }
+
+    /**
+     * @param a any
+     * @param b powerOfTwo
+     * @return
+     */
+    public static int powerOfTwoModel(int a, int b) {
+        return a & (b - 1);
+    }
+
+    /**
+     * 00001000
+     * 11110111
+     * ||
+     * \/
+     * 8
+     *
+     * @param val
+     * @return
+     */
+    private static boolean isPowerOfTwo(int val) {
+        return (val & -val) == val;
+    }
+
+
+    /**
+     * 00001000
+     * 00000111
+     * ||
+     * \/
+     * 0
+     *
+     * @param val
+     * @return
+     */
+    private static boolean isPowerOfTwo_(int val) {
+        return (val & (val - 1)) == 0;
+    }
+
+
+    /**
+     * 求 大于c 得最近得2得幂次方
+     *
+     * @param c
+     * @return
+     */
+    private static final int tableSizeFor(int c) {
+        int n = c - 1;
+        n |= n >>> 1;
+        n |= n >>> 2;
+        n |= n >>> 4;
+        n |= n >>> 8;
+        n |= n >>> 16;
+        return (n < 0) ? 1 : (n >= Integer.MAX_VALUE) ? Integer.MAX_VALUE : n + 1;
     }
 }
---
+++ basic/src/main/java/com/xy/basic/FloatExample.java
@@ -0,0 +1,14 @@
+package com.xy.basic;
+
+/**
+ * @author yefei
+ * @date 2018-02-03 12:51
+ */
+public class FloatExample {
+
+    public static void main(String[] args) {
+        System.out.println(19.9 - 9.9);
+        System.out.println(((byte)-127) << 1);
+    }
+
+}
